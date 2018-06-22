a89878ea4670f15cdc548cda36b1ff24d27e6fd0
manu1919 <34062290+manu1919@users.noreply.github.com>
2018-06-22 23:51:29 +0300

    3. Delegated Transfers

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.4.24;
 contract DappToken {
 
 
-    // token name
     string public name = "CCoin";
-
-    // token symbol
     string public symbol = "CCoin";
-
-    // token standard
     string public standard = "CCoin v1.0.0";
 
     event Transfer(address _from, address _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     // read total number of tokens
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     // constructor
     constructor (uint _initialSupply) public {
@@ -41,7 +38,33 @@ contract DappToken {
 
         // trigger transfer event
         emit Transfer(msg.sender, _to, _value);
-        
+
+        return true;
+    }
+
+    // approve function
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // delegated transfers
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        // update the balances
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // trigger transfer event
+        emit Transfer(_from, _to, _value);
+
         return true;
     }
 }
